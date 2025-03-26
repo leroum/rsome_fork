f14bea36056ef80b0d815c1eaa6e93c086c153b7
len37873 <lena.rosin@umsicht.fraunhofer.de>
2025-03-26 10:26:47 +0100

    Set variable names in SOCProg and model class. Add switch for the lp files

--- rsome/socp.py
+++ rsome/socp.py
@@ -230,7 +230,7 @@ class Model(LPModel):
             formula = super().do_math(primal=True, refresh=False, obj=obj)
             formula = SOCProg(formula.linear, formula.const, formula.sense,
                               formula.vtype, formula.ub, formula.lb,
-                              qmat, formula.obj)
+                              qmat, formula.obj,formula.vname)
             self.primal = formula
             self.pupdate = False
 
@@ -316,9 +316,9 @@ class SOCProg(LinProg):
     The SOCProg class creates an second-order cone program
     """
 
-    def __init__(self, linear, const, sense, vtype, ub, lb, qmat, obj=None):
+    def __init__(self, linear, const, sense, vtype, ub, lb, qmat, obj=None, vname=None):
 
-        super().__init__(linear, const, sense, vtype, ub, lb, obj)
+        super().__init__(linear, const, sense, vtype, ub, lb, obj, vname)
         self.qmat = qmat
 
     def __repr__(self):
@@ -382,9 +382,9 @@ class SOCProg(LinProg):
 
         return table.fillna('-')
 
-    def lp_export(self):
+    def lp_export(self, test=False):
 
-        string = super().lp_export()
+        string = super().lp_export(string = super().lp_export(test))
         index_st = string.find('Subject To')
         s1 = string[:index_st+11]
         s2 = string[index_st+11:]
